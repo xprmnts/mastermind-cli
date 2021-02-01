fdecb3fae1581433598f90f25b6ce737ee3eb9b3
Abhinay Reddy <abhinay@MacBook-Pro.local>
2021-02-01 14:45:52 -0500

    completed validator algorithm, it returns whether guess was success and the total exact and partial matches

--- mastermind.js
+++ mastermind.js
@@ -3,14 +3,16 @@
  */
 const inquirer = require('inquirer');
 const generateSecretCode = require('./bin/generateSecretCode');
+const validateGuess = require('./validateGuess');
 
 // Sample Config
-const _defaultAttempts = 10;
+const _defaultAttempts = 3;
 const _defaultChoices = 6;
 const _defaultPegs = 4;
 
 const _history = [];
 
+//TODO: Pattern Matching to Get Valid Guess
 const question = [
     {
         type: 'string',
@@ -19,32 +21,44 @@ const question = [
     }
 ];
 
-const secret = generateSecretCode(_defaultPegs, _defaultChoices);
+// const secret = generateSecretCode(_defaultPegs, _defaultChoices);
+const secret = [2, 1, 2, 2];
+let attempts = _defaultAttempts;
 
 function game() {
-    inquirer.prompt(question).then(answers => {
-        let playerGuess = answers.guess;
-        // Store Guess
-        _history.push(playerGuess);
+    if (attempts > 0) {
+        inquirer.prompt(question).then(answers => {
+            attempts--;
+            let playerGuess = answers.guess;
+            // Store Guess
+            _history.push(playerGuess);
+            // Check Guess
+            const {
+                codeMatch,
+                exactMatchCount,
+                partialMatchCount
+            } = validateGuess(playerGuess, secret);
 
-        // Check Guess
-        if (playerGuess === '1234') {
-            // Game Over
-            // Draw Table
-            console.log('Game Over');
-        } else if (playerGuess === '4321') {
-            // Partial Match
-            // Draw Table
-            console.log('Partial Matches or No Matches');
-            game();
-        } else {
-            // No Matches
-            // Draw Table
-            console.log('Out of tries');
-        }
-    });
+            console.log(codeMatch, exactMatchCount, partialMatchCount);
 
-    //
+            if (codeMatch) {
+                // Game Over
+                // Draw Table
+                console.log(_history);
+                console.log('Game Over');
+            } else {
+                // Partial Match
+                // Draw Table
+                // console.log(_history);
+                // console.log(secret);
+                console.log('Partial Matches or No Matches');
+
+                game();
+            }
+        });
+    } else {
+        console.log('You ran out of attempts!');
+    }
 }
 
 game();
---
+++ validateGuess.js
@@ -0,0 +1,48 @@
+// Takes player guess and secret code as input
+// returns boolean for complete match, and a number for exact and a number for partial matches
+
+const validateGuess = (guess, code) => {
+    let codeMap = Array(code.length).fill(null);
+    let guessMap = Array(code.length).fill(null);
+
+    // split guess & convert to number
+    let guessArray = guess.split('').map(x => +x);
+
+    for (let i = 0; i < guessArray.length; i++) {
+        if (guessArray[i] === code[i]) {
+            codeMap[i] = 'E';
+            guessMap[i] = 'E';
+        }
+    }
+
+    for (let i = 0; i < guessArray.length; i++) {
+        if (!guessMap[i]) {
+            for (let j = 0; j < code.length; j++) {
+                if (!codeMap[j]) {
+                    if (guessArray[i] === code[j]) {
+                        codeMap[j] = 'P';
+                        guessMap[i] = 'P';
+                    }
+                }
+            }
+        }
+    }
+
+    const codeMatch = codeMap.every(element => element === 'E');
+
+    const exactMatchCount = codeMap.reduce((total, element) => {
+        return element === 'E' ? total + 1 : total + 0;
+    }, 0);
+
+    const partialMatchCount = codeMap.reduce((total, element) => {
+        return element === 'P' ? total + 1 : total + 0;
+    }, 0);
+
+    return {
+        codeMatch,
+        exactMatchCount,
+        partialMatchCount
+    };
+};
+
+module.exports = validateGuess;
