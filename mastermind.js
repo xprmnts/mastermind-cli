c37df97f1c43f16a013275dc008f42bddad31cd3
Abhinay Reddy <abhinay@MacBook-Pro.local>
2021-02-01 12:16:12 -0500

    added inquirer package to get user input, added functionality to ask for user input based on control flow

--- mastermind.js
+++ mastermind.js
@@ -1,3 +1,7 @@
+/*
+ * Packages
+ */
+const inquirer = require('inquirer');
 const generateSecretCode = require('./bin/generateSecretCode');
 
 // Sample Config
@@ -5,4 +9,42 @@ const _defaultAttempts = 10;
 const _defaultChoices = 6;
 const _defaultPegs = 4;
 
-console.log(generateSecretCode(_defaultPegs, _defaultChoices));
+const _history = [];
+
+const question = [
+    {
+        type: 'string',
+        name: 'guess',
+        message: 'enter a code sequence'
+    }
+];
+
+const secret = generateSecretCode(_defaultPegs, _defaultChoices);
+
+function game() {
+    inquirer.prompt(question).then(answers => {
+        let playerGuess = answers.guess;
+        // Store Guess
+        _history.push(playerGuess);
+
+        // Check Guess
+        if (playerGuess === '1234') {
+            // Game Over
+            // Draw Table
+            console.log('Game Over');
+        } else if (playerGuess === '4321') {
+            // Partial Match
+            // Draw Table
+            console.log('Partial Matches or No Matches');
+            game();
+        } else {
+            // No Matches
+            // Draw Table
+            console.log('Out of tries');
+        }
+    });
+
+    //
+}
+
+game();
